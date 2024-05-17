8958b50e353595cee3b7f6f3e3016b2ffdddc276
William <william.edwardo@gmail.com>
2024-05-16 23:53:49 -0400

    second commit

---
+++ src/main/java/wiledw/MovieMeter/Movie.java
@@ -0,0 +1,29 @@
+package wiledw.MovieMeter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // For getter & setter
+@AllArgsConstructor // For Movie constructor containing attributes
+@NoArgsConstructor  // For Movie constructor with empty attributes
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference // Only store the reviewIds instead the full review, all reviews will be placed in the review collection
+    private List<Review> reviewIds;
+}
---
+++ src/test/java/wiledw/MovieMeter/MovieMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package wiledw.MovieMeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
